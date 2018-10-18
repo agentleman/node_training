6fbbe1912231cd60f5a5a3845f53ddaca9340bcf
agentleman <675740051@qq.com>
2018-10-18 20:03:23 +0800

    ionic_app

--- ionic/ionicdemo/src/app/app.module.ts
+++ ionic/ionicdemo/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZidigyiComponent } from './zidigyi/zidigyi.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ZidigyiComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
